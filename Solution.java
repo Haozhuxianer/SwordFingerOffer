ec8c5af8ded085a2035aa7baffd92d31bef95bb7
Hzxr_ <13277250082@163.com>
2017-12-05 09:44:53 +0800

    first commit

---
+++ Solution.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.lang.Math;
+public class Solution {
+    public ArrayList<Integer> FindNumbersWithSum(int[] array, int sum) {
+        int low = array.length / 2;
+        int high = array.length / 2 + 1;
+        int cur = array[low] + array[high];
+        ArrayList result = new ArrayList<Integer>();
+        while(low < high) {
+	    
+     }
+        
+
+    public static void main(String[] args) {
+        Solution s  = new Solution();
+        int[] array = {1,2,4,7,11,15};
+        System.out.println(s.FindNumbersWithSum(array, 15));
+    }
+}
